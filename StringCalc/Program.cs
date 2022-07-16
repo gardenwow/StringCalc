41a99f6b61ee4dd3d35110ca4eb4069642571196
seregaffm17@gmail.com <seregaffm17@gmail.com>
2022-07-16 11:27:33 +0400

    Добавьте файлы проекта.

---
+++ StringCalc/Program.cs
@@ -0,0 +1,153 @@
+﻿namespace StringCalculator
+
+{
+    class Calc
+    {
+
+        static public double Calculate(string input)
+        {
+            string output = GetExpression(input);
+            double result = Counting(output);
+            return result;
+        }
+        static private string GetExpression(string input)
+        {
+            string output = null;
+            Stack<char> operStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (IsDelimeter(input[i]))
+                    continue;
+
+                if (Char.IsDigit(input[i]))
+                {
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i];
+                        i++;
+
+                        if (i == input.Length) break;
+                    }
+
+                    output += " ";
+                    i--;
+                }
+
+
+                if (IsOperator(input[i]))
+                {
+                    if (input[i] == '(')
+                        operStack.Push(input[i]);
+                    else
+                    if (input[i] == ')')
+                    {
+
+                        char s = operStack.Pop();
+
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (operStack.Count > 0)
+                            if (Prioritet(input[i]) <= Prioritet(operStack.Peek()))
+                                output += operStack.Pop().ToString() + " ";
+                        operStack.Push(char.Parse(input[i].ToString()));
+
+                    }
+                }
+            }
+
+
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output;
+        }
+        static private double Counting(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        a += input[i];
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a));
+                    i--;
+                }
+                else if (IsOperator(input[i]))
+                {
+
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+
+                    switch (input[i])
+                    {
+                        case '+': result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': result = b * a; break;
+                        case '/': result = b / a; break;
+                    }
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek();
+        }
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        static private byte Prioritet(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                default: return 5;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("vvedite to chto hyjno vichislit: ");
+                
+                Console.WriteLine($"itog: {Calc.Calculate(Console.ReadLine())}");
+                
+            }
+        }
+    }
+}
